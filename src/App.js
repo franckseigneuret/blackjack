dccbce5798e5a4866627314c0f0af847690fa9ce
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-05-19 16:05:53 +0200

    load des premières cartes pour chancun

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
   const [firstname, setFirstname] = useState(null)
   const [part, setPart] = useState(null)
   const [deckId, setDeckId] = useState(null)
+  const [draw, setDraw] = useState(false)
+  const [score, setScore] = useState({
+    bank: {
+      cards: [],
+      total: 0,
+    },
+    player: {
+      cards: [],
+      total: 0,
+    },
+  })
 
   const handleName = (e) => {
     setFirstname(e.currentTarget.value)
@@ -17,7 +28,7 @@ function App() {
   }
 
   useEffect(() => {
-
+    // load des cartes à jouer
     isFirstnameDefined && fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
       .then((r) => r.json())
       .then(datas => {
@@ -26,6 +37,21 @@ function App() {
       })
 
   }, [isFirstnameDefined])
+
+  useEffect(() => {
+    // distribution des premières cartes (2 pour la bank et 2 pour le player)
+    isFirstnameDefined && fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
+      .then((r) => r.json())
+      .then(datas => {
+        const scoreTemp = { ...score }
+        console.log('d=', datas.cards)
+        // fill 2 cards for player
+        scoreTemp.player.cards = datas.cards.slice(0, 2)
+        // fill 2 cards for bank
+        scoreTemp.bank.cards = datas.cards.slice(2)
+        setScore(scoreTemp)
+      })
+  }, [deckId])
 
   return (
     <div className="App">
@@ -37,8 +63,36 @@ function App() {
       }
 
       {
-        isFirstnameDefined && firstname && <p>Bonjour {firstname}</p>
+        isFirstnameDefined && firstname && <>
+          <div>
+            <p>Bonjour {firstname}</p>
+            <p>deck_id : {deckId}</p>
+            <p>link : https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=4</p>
+
+            <p>Votre jeu :</p>
+            <ul>
+              {
+                score.player.cards && score.player.cards.map(c => (
+                  <li><img src={c.image} alt={c.code} /></li>
+                ))
+              }
+            </ul>
+          </div>
+          <div>
+            <p>Le jeu de la banque :</p>
+            <ul>
+              {
+                score.bank.cards && score.bank.cards.map(c => (
+                  <li><img src={c.image} alt={c.code} /></li>
+                ))
+              }
+            </ul>
+          </div>
+          
+        </>
       }
+
+
 
     </div>
   );
