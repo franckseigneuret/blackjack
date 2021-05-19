f3cf6c268fc4d931519ba6cd0283eba51fc83154
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-05-19 15:30:10 +0200

    nom user + fetch 1ere partie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+
 import './App.css';
 
 function App() {
+  const [isFirstnameDefined, setIsFirstnameDefined] = useState(false)
+  const [firstname, setFirstname] = useState(null)
+  const [part, setPart] = useState(null)
+  const [deckId, setDeckId] = useState(null)
+
+  const handleName = (e) => {
+    setFirstname(e.currentTarget.value)
+  }
+
+  const handleFirstnameDefined = (e) => {
+    setIsFirstnameDefined(true)
+  }
+
+  useEffect(() => {
+
+    isFirstnameDefined && fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
+      .then((r) => r.json())
+      .then(datas => {
+        setPart(datas)
+        setDeckId(datas.deck_id)
+      })
+
+  }, [isFirstnameDefined])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        !isFirstnameDefined && <>
+          <input type="text" name="firstname" onChange={handleName} />
+          <button onClick={handleFirstnameDefined}>Votre nom</button>
+        </>
+      }
+
+      {
+        isFirstnameDefined && firstname && <p>Bonjour {firstname}</p>
+      }
+
     </div>
   );
 }
